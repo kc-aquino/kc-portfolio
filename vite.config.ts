5ba0ba9feddc142c1f4b583cd61844fd14761405
crystal <217715354+crystal-go8@users.noreply.github.com>
2025-10-15 12:24:03 +0800

    retract that

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kc-portfolio/', // <-- GitHub repo name
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.glb'],
   resolve: {
